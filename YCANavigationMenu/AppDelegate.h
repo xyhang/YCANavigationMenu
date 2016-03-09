7d92b5670f6b2dbe4c9214a9ab995ba5a5e1e2f4
yan <yan@yandemacbook-pro.local>
2016-03-09 16:49:46 +0800

    Initial Commit

---
+++ YCANavigationMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCANavigationMenu
+//
+//  Created by yan on 16/3/9.
+//  Copyright © 2016年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCANavigationMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCANavigationMenu
+//
+//  Created by yan on 16/3/9.
+//  Copyright © 2016年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
